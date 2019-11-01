0483c9960b317c8075ec2be936936acfd93395be
BcCinema <jjfiles19@gmail.com>
2019-11-01 12:11:50 -0500

    unpacked v4. Added labels.txt

--- app/src/main/java/com/example/mood/MainActivity.java
+++ app/src/main/java/com/example/mood/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.mood;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_DARK_THEME = "dark_theme";
+
     private Button btnForward; //button object created for the forwardBtn on activity_main
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //use the chosen theme
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useDarkTheme = preferences.getBoolean(PREF_DARK_THEME, false);
+
+        if (useDarkTheme) {
+            setTheme(R.style.DarkTheme);
+        }
+
+        ////////////////////////////////////////
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //for Switch Theme Toggle
+        Switch toggle = findViewById(R.id.themeSwitch);
+        toggle.setChecked(useDarkTheme);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                toggleTheme(isChecked);
+            }
+        });
 
         Button submitBtn = (Button) findViewById(R.id.submitBtn); //this will handle the GET MOODY
         //button once facial recognition is implemented
@@ -33,6 +61,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }); //handles the event for when you click the settings button on activity_main
     }
+        //Used to Save User's Theme choice
+        public void toggleTheme(boolean darkTheme){
+            SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+            editor.putBoolean(PREF_DARK_THEME, darkTheme);
+            editor.apply();
+
+            Intent intent = getIntent();
+            finish();
+
+            startActivity(intent);
+        }
+
+        /////////////////////////////////////////////////
+
     private void moveToActivityTwo(){ //creates the event when you click the settings button on
         //activity main
         Intent intent = new Intent(MainActivity.this, settings.class );
--- app/src/main/java/com/example/mood/settings.java
+++ app/src/main/java/com/example/mood/settings.java
@@ -1,19 +1,47 @@
 package com.example.mood;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class settings extends AppCompatActivity {
 
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_DARK_THEME = "dark_theme";
+
+
     private Button btnPrevious; //created a button object to use to call our button id
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //use the chosen theme
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useDarkTheme = preferences.getBoolean(PREF_DARK_THEME, false);
+
+        if (useDarkTheme) {
+            setTheme(R.style.DarkTheme);
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //for Switch Theme Toggle
+        Switch toggle = findViewById(R.id.settingsThemeSwitch);
+        toggle.setChecked(useDarkTheme);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                toggleTheme(isChecked);
+            }
+        });
+
 
         btnPrevious = findViewById(R.id.homeBtn); //this gets the button's id and stores it in btnPrevious
         btnPrevious.setOnClickListener(new View.OnClickListener(){
@@ -24,6 +52,19 @@ public class settings extends AppCompatActivity {
         }); //this is an event handler that tells what happens when btnPrevious (homeBtn) is clicked
         //in the settings activity
     }
+
+    //Used to Save User's Theme choice
+    public void toggleTheme(boolean darkTheme){
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putBoolean(PREF_DARK_THEME, darkTheme);
+        editor.apply();
+
+        Intent intent = getIntent();
+        finish();
+
+        startActivity(intent);
+    }
+
     private void moveToActivityOne(){ //handles/creates the event
         Intent intent = new Intent(settings.this, MainActivity.class );
         startActivity(intent);
