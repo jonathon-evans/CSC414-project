d414fa1bb18bd9614e579ad25456bc323215a936
Jeffery Files <jjfiles19@gmail.com>
2019-11-21 18:32:18 -0600

    Split into integratoin testing branch

--- app/src/main/java/com/example/mood/MainActivity.java
+++ app/src/main/java/com/example/mood/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.mood;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -19,8 +24,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-
-
+/*import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D; */
+import android.graphics.Bitmap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -101,6 +109,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageTmp = (Bitmap) extras.get("data");
+
+            Bitmap imageBitmap = Bitmap.createScaledBitmap(imageTmp, 48, 48, true);
+            imageBitmap = toGrayscale(imageBitmap);
+            //imageView.setImageBitmap(imageBitmap);
+            Integer reeeeee = null;
+            tflite.run(imageBitmap, reeeeee); //code to bite the image
+            System.out.write(reeeeee);
+        }
+    }
+
+    public Bitmap toGrayscale(Bitmap bmpOriginal)
+    {        
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
 
     //Used to Save User's Theme choice
     public void toggleTheme(boolean darkTheme){
@@ -114,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-        /////////////////////////////////////////////////
+    /////////////////////////////////////////////////
 
     private void moveToActivityTwo(){ //creates the event when you click the settings button on
         //activity main
@@ -126,8 +164,8 @@ public class MainActivity extends AppCompatActivity {
         Intent moveMusicIntent = new Intent(MainActivity.this, music.class);
         startActivity(moveMusicIntent);
     }
-    public MappedByteBuffer loadModelFile() throws IOException
-    {
+
+    public MappedByteBuffer loadModelFile() throws IOException {
         AssetFileDescriptor fileDescriptor=this.getAssets().openFd("converted_ERCNN.tflite");
         FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
         FileChannel fileChannel=inputStream.getChannel();
@@ -135,7 +173,59 @@ public class MainActivity extends AppCompatActivity {
         long declaredLength=fileDescriptor.getDeclaredLength();
         return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,declaredLength);
     }
-
-
-
 }
+/*
+public class ProcessImage {
+
+	public static void main(String args[])throws IOException{
+
+	BufferedImage inputImg = null; //holds input image file
+	File f = null; //holds image path
+
+	//read image
+	try {
+		f = new File("C:\\Users\\asiaa\\Documents\\test.jpg");//insert input image path here
+		inputImg = ImageIO.read(f);
+	}catch(IOException e) {
+	System.out.println(e);
+	}
+
+	//set image dimensions to 48x48
+	BufferedImage outputImg = new BufferedImage(48, 48, inputImg.getType()); //creates output image
+	Graphics2D g2d = outputImg.createGraphics();
+	g2d.drawImage(inputImg,  0, 0, 48,  48,  null);
+	g2d.dispose();
+
+	//loop to process each pixel
+	for(int y = 0; y < 48; y++) {
+		for(int x = 0; x < 48; x++) {
+
+			//variables to hold pixel, alpha, red, green, and blue value
+			int p = outputImg.getRGB(x,y);
+			int a = (p>>24)&0xff;
+			int r = (p>>16)&0xff;
+			int g = (p>>8)&0xff;
+			int b = p&0xff;
+
+			//average of RGB value
+			int avg = (r+g+b)/3;
+
+			//find new pixel value and set it to the pixel at (x,y)
+			p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+			outputImg.setRGB(x, y, p);
+		}
+	}
+
+		//writing the image file
+		try {
+			f = new File("C:\\Users\\asiaa\\Documents\\output.jpg"); //path for processed image goes here
+			ImageIO.write(outputImg, "jpg", f);
+		}catch(IOException g) {
+			System.out.println(g);
+		}
+
+	}//main ends
+
+}//class ends
+
+*/
